db3cd665a80b6cc3547c68d01fcedf7561ecf42b
Alan deLevie <adelevie@gmail.com>
2011-05-29 23:31:19 -0400

    removed cruft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
   before_filter :instantiate_action_javascript
-
-  def instantiate_action_javascript
-    js_path = File.join(controller_path, "#{action_name}.js")
-    @current_action_javascript_path = File.exist?(File.join(RAILS_ROOT, 'public', 'javascripts', js_path)) ? js_path : nil
-  end
 end
