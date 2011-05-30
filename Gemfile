0c63f0977494c973ed59d74ead37004ff13ac5ae
Alan deLevie <adelevie@gmail.com>
2011-05-30 02:26:46 -0400

    eventmachine + pusher working together

--- Gemfile
+++ Gemfile
@@ -14,6 +14,23 @@ gem 'pusher'
 gem 'jquery-rails'
 gem 'sinatra'
 gem 'thin'
+gem 'em-http-oauth-request'
+
+# async wrappers
+gem 'eventmachine',     :git => 'git://github.com/eventmachine/eventmachine.git'
+#gem 'rack-fiber_pool',  :require => 'rack/fiber_pool'
+#gem 'em-synchrony',     :git => 'git://github.com/igrigorik/em-synchrony.git', :require => [
+#  'em-synchrony',
+#  'em-synchrony/em-http'
+#  ]
+
+# async activerecord requires
+#gem 'mysql2'
+
+# async http requires
+gem 'em-http-request',:git => 'git://github.com/igrigorik/em-http-request.git', :require => 'em-http'
+
+#gem 'addressable', :require => 'addressable/uri'
 
 
 # Use unicorn as the web server
--- app/metal/tweet.rb
+++ app/metal/tweet.rb
@@ -1,7 +1,43 @@
 class Tweet < Sinatra::Base
 
   get '/twitter_em_server' do
-    "sinatra"
+
+    user = ""
+    password = ""
+
+    #url = 'http://stream.twitter.com/1/statuses/sample.json'
+    url = 'http://stream.twitter.com/1/statuses/mentions.json'
+
+    def handle_tweet(tweet)
+      return unless tweet['text']
+      puts "#{tweet['user']['screen_name']}: #{tweet['text']}"
+
+
+      answerer = Answerer.find_or_create_by_username(tweet['user']['screen_name'])
+
+      SubmittedAnswer.new do |sa|
+        sa.body = tweet['text']
+        sa.twitter_id_str = tweet['id_str']
+        sa.quiz_id = 99999
+        sa.question_id = 1234
+        sa.answerer_id = answerer.id
+      end.save
+
+    end
+
+    EventMachine.run do
+      http = EventMachine::HttpRequest.new(url).get :head => { 'Authorization' => [ user, password ] }
+
+      buffer = ""
+
+      http.stream do |chunk|
+        buffer += chunk
+        while line = buffer.slice!(/.+\r?\n/)
+          handle_tweet JSON.parse(line)
+        end
+      end
+    end
   end
 
-end  
+end
+
